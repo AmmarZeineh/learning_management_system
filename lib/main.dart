292113b786ff0d1c2ceb5cd7ce2798fb6e3618f3
ammarzeineh <ammarzeineh3@gmail.com>
2024-05-25 17:12:18 +0300

    add views folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const LearningManagementSystem());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LearningManagementSystem extends StatelessWidget {
+  const LearningManagementSystem({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
+      home: WelcomeView(),
+    );
+  }
+}
+
+class WelcomeView extends StatelessWidget {
+  const WelcomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [],
     );
   }
 }
